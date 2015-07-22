8dfd6f167fa651bf7b7a535516cdbfa8075a7a97
Lucas Cartaxo <lucascartaxo@live.com>
2015-07-22 00:26:49 -0300

    start to put NavigationDrawer to work

--- app/src/main/java/com/uhuraapp/uhura/Main.java
+++ app/src/main/java/com/uhuraapp/uhura/Main.java
@@ -1,16 +1,31 @@
 package com.uhuraapp.uhura;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class Main extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class Main extends Activity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private ListView drawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerList = (ListView) findViewById(R.id.left_drawer);
+
+        drawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new ArrayList<String>()));
     }
 
     @Override
